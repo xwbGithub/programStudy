1f5dc01edff4e62ef90225df09ff951cc1057c42
crown <1056481167@qq.com>
2020-03-24 23:45:12 +0800

    file commit

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -9,9 +9,30 @@ Page({
   data: {
     detailObj:{},
     index:null,
-    isCollected:false
+    isCollected:false,
+    isMusicPlay:false
+  },
+  
+  //------------------处理音乐播放功能------------------
+  handleMusicPlay(){
+    let isMusicPlay = !this.data.isMusicPlay;
+    this.setData({ 
+      isMusicPlay
+      });
+    //控制音乐播放
+    if (isMusicPlay){
+      //音乐播放
+      let {dataUrl,title}=this.data.detailObj.music;
+      wx.playBackgroundAudio({
+        dataUrl, title
+      })
+    }else{
+      //暂停播放
+      wx.pauseBackgroundAudio();
+    }
   },
 
+  //------------------处理音乐播放功能------------------
   /**
    * 生命周期函数--监听页面加载
    */
@@ -21,13 +42,21 @@ Page({
     //更新data中detailObj的状态值
     this.setData({
       detailObj: datas.list_data[index]
+    });
+    //根据本地缓存的额数据判断用户是否收藏过当前文章
+    let detailStorage=wx.getStorageSync("isCollected");
+    console.log(detailStorage);
+    if (detailStorage[index]){//收藏过
+    this.data({
+      isCollected:true
     })
+    }
   },
   //收藏和没有收藏按钮
   handleCollection(){
     console.log("是否收藏this-->"+this);
-    let thisIsCollected = !this.data.isCollected;
-    let whetherTitle = thisIsCollected==false?"取消收藏成功":"收藏成功";
+    let isCollected = !this.data.isCollected;
+    let whetherTitle = isCollected==false?"取消收藏成功":"收藏成功";
     //弹出提示框
     wx.showToast({
       title: whetherTitle,
@@ -35,55 +64,23 @@ Page({
     })
     //更新收藏按钮的点击状态
     this.setData({
-      isCollected: thisIsCollected
+      isCollected
+    })
+    let {index}=this.data;
+    wx.getStorage({
+      key: 'isCollected',
+      success: (datas)=> {
+        let obj=datas.data;
+        obj[index]=datas.data;
+        obj[index]=isCollected;
+        wx.setStorage({
+          key: 'isCollected',
+          data: obj,
+          success:()=>{
+            console.log("缓存成功")
+          }
+        })
+      },
     })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
